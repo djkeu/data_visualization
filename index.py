20091ad61f49a94795b12365a9dac7137bf43d6b
djkeu <mrkooij@gmail.com>
2023-10-19 14:21:50 +0200

    Defining custom colors

--- index.py
+++ index.py
@@ -3,16 +3,18 @@
 # Chapter 15: Generating data, p.305
 
 """
+Note: 'q' closes figure
+
 Installing MatplotLib, p.306
 Plotting a simple line graph, p.306
         mpl_squares.py
     Changing the label type and line thickness, p.307
     Correcting the plot, p.309
-    Note: 'q' closes figure
     Using built-in styles, p.310
     Plotting and styling individual points with scatter(), p.310
         scatter_squares.py
     Plotting a series of points with scatter(), p.312
     Calculating data automatically, p.312
-    
+    Defining custom colors, p.314
+        
 """
