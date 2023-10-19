1475c1a94167cc5a81000f2895b4d72db984a454
djkeu <mrkooij@gmail.com>
2023-10-19 18:24:20 +0200

    Plot first five cubic numbers

--- cubes.py
+++ cubes.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 x_values = range(1, 6)
 y_values = [x **3 for x in x_values]
 
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(x_values, y_values)
+
+plt.show()
