c6e8abc1a9c8e52b53c6278f7bdef71a5bf3a961
djkeu <mrkooij@gmail.com>
2023-10-19 21:49:24 +0200

    Multiple walks

--- rw_visual.py
+++ rw_visual.py
@@ -3,14 +3,21 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-# Make a random walk
-rw =RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as the program is active
+while True:
+    # Make a random walk
+    rw =RandomWalk()
+    rw.fill_walk()
 
-# Plot the points in the walk
-plt.style.use('seaborn-v0_8')  # 'classic' doesn't close with 'q'
+    # Plot the points in the walk
+    plt.style.use('seaborn-v0_8')  # 'classic' doesn't close with 'q'
 
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
 
-plt.show()
+    plt.show()
+
+    keep_running = input("Nog een eindje lopen? (j/n): ")
+    if keep_running == 'n':
+        break
+    
