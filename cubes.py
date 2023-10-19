7f32741ee92c13350cc95f1da8bdd7fae488cce8
djkeu <mrkooij@gmail.com>
2023-10-19 18:26:21 +0200

    Plot first 5000 cubic numbers

--- cubes.py
+++ cubes.py
@@ -3,7 +3,8 @@
 
 import matplotlib.pyplot as plt
 
-x_values = range(1, 6)
+# x_values = range(1, 6)
+x_values = range(1, 5001)
 y_values = [x **3 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
