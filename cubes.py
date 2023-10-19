00ed941e642dc27fc914f0fc4d0d29f4d31b37fb
djkeu <mrkooij@gmail.com>
2023-10-19 18:17:04 +0200

    Create x_values and y_cubes

--- cubes.py
+++ cubes.py
@@ -1,3 +1,10 @@
 # ToDo: 15.1 Cubes
 # A number raised to the third power is a cube. Plot the first five cubic numbers, and then plot the first 5000 cubic numbers.
 
+import matplotlib.pyplot as plt
+
+x_values = range(1, 6)
+y_values = [x **3 for x in x_values]
+for value in y_values:
+    print(value)
+
