7050a07ab24175c6ca6d2537844ccac13c0a8ccd
djkeu <mrkooij@gmail.com>
2023-10-19 19:40:48 +0200

    Create and initialize RandomWalk()

--- random_walk.py
+++ random_walk.py
@@ -1,1 +1,13 @@
 from random import choice
+
+class RandomWalk:
+    """Class to generate random walks."""
+
+    def __init__(self, num_points=5000) -> None:
+        """Initialize attributes of a walk."""
+        self.num_points = num_points
+
+        # All walks start at (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
+        
