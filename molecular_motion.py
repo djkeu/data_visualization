e43fe35181f19d42837e9b40a02db7b708f3ab50
djkeu <mrkooij@gmail.com>
2023-10-20 11:21:02 +0200

    Stop looping the walks

--- molecular_motion.py
+++ molecular_motion.py
@@ -3,29 +3,23 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-# Keep making new walks, as long as the program is active
-while True:
-    # Make a random walk
-    rw =RandomWalk(5_000)
-    rw.fill_walk()
+# Make a random walk
+rw =RandomWalk(5_000)
+rw.fill_walk()
 
-    # Plot the points in the walk
-    plt.style.use('seaborn-v0_8')  # 'classic' doesn't close with 'q'
+# Plot the points in the walk
+plt.style.use('seaborn-v0_8')  # 'classic' doesn't close with 'q'
 
-    fig, ax = plt.subplots(figsize=(13,7))
-    point_numbers = range(rw.num_points)
-    ax.plot(rw.x_values, rw.y_values, linewidth='0.7')
+fig, ax = plt.subplots(figsize=(13,7))
+point_numbers = range(rw.num_points)
+ax.plot(rw.x_values, rw.y_values, linewidth='0.7')
 
-    # Emphasize the first and last points
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+# Emphasize the first and last points
+ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
-    # Remove the axes
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+# Remove the axes
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
 
-    plt.show()
-
-    keep_running = input("\nNog een eindje lopen? \n('n' om te stoppen): ")
-    if keep_running == 'n':
-        break
+plt.show()
