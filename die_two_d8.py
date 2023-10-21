a974ee78eaaaaeacf59eb5f074de567fe3e3277f
djkeu <mrkooij@gmail.com>
2023-10-21 13:12:28 +0200

    Adjust my_layout title

--- die_two_d8.py
+++ die_two_d8.py
@@ -31,7 +31,7 @@ data = Bar(x=x_values, y=frequencies)
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 
-my_layout = Layout(title="Results of rolling a D6 and a D10 50000 times",
+my_layout = Layout(title="Results of rolling two D8's 1000 times",
 xaxis=x_axis_config, yaxis=y_axis_config)
 
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d8_d8.html')
