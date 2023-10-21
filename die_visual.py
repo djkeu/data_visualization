8b0fda99e7c551f29c011a45b77b21601fdcf2b2
djkeu <mrkooij@gmail.com>
2023-10-21 12:23:19 +0200

    Visualize the results

--- die_visual.py
+++ die_visual.py
@@ -21,3 +21,11 @@ for value in range(1, die.num_sides+1):
 
 print(frequencies)
 
+# Visualize the results
+x_values = list(range(1, die.num_sides+1))
+data = Bar(x=x_values, y=frequencies)
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title="Results of rolling one D6 1000 times", xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
