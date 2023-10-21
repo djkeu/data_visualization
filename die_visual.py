a23fda76ebafd4b57c1ea199197baed4b53af9a6
djkeu <mrkooij@gmail.com>
2023-10-21 12:12:20 +0200

    Roll die 1000x, remove print()

--- die_visual.py
+++ die_visual.py
@@ -6,8 +6,7 @@ die = Die()
 # Make rolls, store the results
 results = []
 
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
