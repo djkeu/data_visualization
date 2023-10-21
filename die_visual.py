13189be755dcb40df6af061e715348899b25bcc9
djkeu <mrkooij@gmail.com>
2023-10-21 12:14:55 +0200

    Analyze and print frequencies

--- die_visual.py
+++ die_visual.py
@@ -10,3 +10,10 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
+# Analyze the results
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
