de3a57c8c4c09b83b5afeefc2d1de18bfce754bc
djkeu <mrkooij@gmail.com>
2023-10-23 17:57:17 +0200

    Generate rolls inline, remove import Die()

--- mpl_die.py
+++ mpl_die.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
+from random import randint
 
-from die import Die
+fnum_sides = 6
 
-die = Die()
-
-input_values = range(1, die.num_sides+1)
+input_values = range(1, num_sides+1)
 
 rolls = []
 throws = 101
-for roll in range(1, throws):
-    side = die.roll()
-    rolls.append(side)
+for throw in range(1, throws):
+    roll = randint(1, num_sides+1)
+    rolls.append(roll)
 
 results = []
-max_result = die.num_sides
+max_result = num_sides
 for roll in range(1, max_result+1):
     result = rolls.count(roll)
     results.append(result)
@@ -25,7 +24,7 @@ fix, ax = plt.subplots()
 fmt = 'bo'
 ax.plot(input_values, results, fmt)
 
-ax.set_title(f"Resultaten van {throws} keer gooien met een dobbelsteen:")
+ax.set_title(f"Resultaten van {throws - 1} x gooien met een dobbelsteen:")
 ax.set_xlabel("Kanten van de dobbelsteen")
 ax.set_ylabel("Aantal keer gegooid")
 
