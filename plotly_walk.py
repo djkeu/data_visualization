fd34b3bf37553b1dde6754981b31f1b538147658
djkeu <mrkooij@gmail.com>
2023-10-23 21:50:35 +0200

    Finish plotly_walk somehow

--- plotly_walk.py
+++ plotly_walk.py
@@ -1,6 +1,10 @@
 import plotly.express as px
-from plotly import offline
 
 from random_walk import RandomWalk
 
+pw = RandomWalk(50_000)
+pw.fill_walk()
 
+fig = px.scatter(pw.x_values, pw.y_values)
+fig.update_traces(marker_size = 1)
+fig.show()
